c5562351e831966056656dd96f3be36bd39374f0
Michael Scott <33077129+ThreatLevelMidnight1234@users.noreply.github.com>
2017-11-13 15:51:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # company_code_repo
-print "This is a test script!"
+#This is the password to get into our test servers. You can run the code at https://repl.it/languages/python3
+def get_flag():
+	flag=[102, 108, 97, 103, 123, 99, 111, 100, 101, 95, 114, 101, 112, 111, 115, 95, 99, 97, 110, 95, 104, 97, 118, 101, 95, 103, 114, 101, 97, 116, 95, 105, 110, 102, 111, 125]
+	print(''.join(chr(i) for i in flag))
+
+get_flag()
